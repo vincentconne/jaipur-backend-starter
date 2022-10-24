e1a62b414f1e1389a98ff4715486b5e1ae5706eb
viconne <viconne@a112pc12L.local.isima.fr>
2022-10-24 18:36:20 +0200

    jeux de tests

--- src/services/gameService.test.js
+++ src/services/gameService.test.js
@@ -1,6 +1,6 @@
 import * as gameService from "./gameService"
 
-describe("Game service", () => {
+/*describe("Game service", () => {
   test("should put camels from hand to herd", () => {
     const game = {
       _players: [
@@ -36,6 +36,39 @@ describe("Game service", () => {
   ///
   const deck = gameService.initDeck()
   expect(deck.length).toBe(52)
+})*/
+
+describe("Game service", () => {
+  test("should init a deck", () => {
+    const defaultDeck = gameService.initDeck()
+    expect(defaultDeck.length).toBe(52)
+    expect(defaultDeck.filter((card) => card === "diamonds").length).toBe(6)
+    // etc
+  })
+
+  test("should draw cards", () => {
+    const deck = ["camel", "diamonds", "gold"]
+    const drawnCard = gameService.drawCards(deck, 1)
+    expect(deck.length).toBe(2)
+    expect(drawnCard).toStrictEqual(["camel"])
+  })
+
+  test("should put camels from hand to herd", () => {
+    const game = {
+      _players: [
+        { hand: ["camel", "gold"], camelsCount: 0 },
+        { hand: ["gold", "gold"], camelsCount: 0 },
+      ],
+    }
+    gameService.putCamelsFromHandToHerd(game)
+    expect(game._players[0].hand.length).toBe(1)
+    expect(game._players[0].hand).toStrictEqual(["gold"])
+    expect(game._players[0].camelsCount).toBe(1)
+
+    expect(game._players[1].hand.length).toBe(2)
+    expect(game._players[1].hand).toStrictEqual(["gold", "gold"])
+    expect(game._players[1].camelsCount).toBe(0)
+  })
 })
 
 // npm run test
